6eeb86234ba3cf4b7627c767bda527ad08637a1e
Serra <pacoserranox@gmail.com>
2021-11-11 15:37:16 +0100

    Add project files.

---
+++ PrimerCore/Controllers/PruebaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //ESTAMOS INDICANDO QUE TENDREMOS UNA VISTA 
+        //LLAMADA Index DENTRO DE Views/Prueba
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
